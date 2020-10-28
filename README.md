f8112ec5bae8f94765e875578a6303b1d1f1999a
wuaoxiang <pylint@yandex.com>
2020-10-28 14:45:56 +0800

    add graphql curl with variables example

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 GraphQL isn't tied(绑定) to any specific database or store engine
 
-## Schema
+## schema简介
 
 想用graphql就必须要有一个全局变量/单例模式Schema(例如用OnceCell wrap Schema)
 
@@ -12,9 +12,38 @@ Query部分可以理解为CRUD的R，Mutation则包括对数据的增删改，Su
 
 use async_graphql::Object之后就会包含Object和field两个过程宏(filed过程宏在2.0版本已被删除?，改名为#[graphql])
 
+## types
+
+grapqhl basic scalar types: String, Int(i32), Float(f32), Boolean, and ID
+
+类型后接个叹号表示NonNull，所以如果下面例子中的value是Optional，则类型Float后面不会接上叹号
+
+```
+type ValueAndChangeRate {
+  value: Float!
+  changeRate: Float!
+}
+```
+
+## Graphql通信
+
+Request和Response的消息格式都是: Content-Type: application/json
+
+Request会用POST请求将json数据携带在payload中: `{query: "{hello}", variables: null}`
+
+### warp路由
+
+一定要先写playground再or post，`graphql_post.or(graphql_playground)`这种写法连playground页面都进不了
+
 ---
 
-# English words
+## English words
+
+### English(graphql)
+
+- exclamation point: 感叹号
+
+### English(other)
 
 - criminal and civil: 刑事和民事
 - handicapped: 残疾地
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use async_graphql::{Object, Schema, Context, EmptySubscription, EmptyMutation};
-use std::sync::Arc;
-// use tokio::sync::Mutex;
-use futures::lock::Mutex;
+use async_graphql::{Object, Schema, EmptySubscription, EmptyMutation};
 use std::convert::Infallible;
 use warp::{http::Response as HttpResponse, Filter, Rejection};
 use async_graphql_warp::{BadRequest, Response};
@@ -10,67 +7,27 @@ use async_graphql::http::{GraphQLPlaygroundConfig, playground_source};
 
 type DynError = Result<(), Box<dyn std::error::Error>>;
 
-#[derive(Default)]
-struct MyFirstQuery;
+struct QueryRoot;
 
-/// impl resolver function for QueryRoot. a SimpleObject can't have resolver function(因为自动生成了各字段的getter方法)
+/// impl resolver function for QueryRoot
 #[Object]
-impl MyFirstQuery {
-    /**
-    {add(a: 10, b: 20)}
-    */
+impl QueryRoot {
+    /// {add(a:10, b:20)}: 如果没有携带变量参数，最外层的query关键字可以省略
+    /// `query($a: Int!) {add(a: $a, b: 2)}` + {"a":123}: 如果含变量，必须要在最外层的花括号外的写上query()去「定义变量及其类型」
+    /// curl -X POST -H "Content-Type: application/json" -d '{"query": "{add(a:1,b:1)}"}' http://localhost:8003
+    /// curl -X POST -H "Content-Type: application/json" -d '{"query": "query{add(a:1,b:1)}", "variables": null}' http://localhost:8003
+    /// curl -X POST -H "Content-Type: application/json" -d '{"query": "query($a: Int!){add(a:$a,b:1)}", "variables": {"a": 1}}' http://localhost:8003
     async fn add(&self, #[graphql(desc = "Returns the sum of a and b")] a: i32, b: i32) -> async_graphql::Result<i32> {
         Ok(a + b)
     }
 
-    /**
-    {
-      signUp(newUser: { email: "w@w.w", password: "1234" }) {
-        id
-        email
-      }
+    /// sign_up会转为js的驼峰命名，枚举命名FileA则会转为js风格例如FILE_A
+    async fn sign_up(&self, _new_user: NewUser) -> User {
+        // TODO
+        User::default()
     }
-    */
-    async fn sign_up(&self, new_user: NewUser) -> User {
-        User {
-            id: 0,
-            email: new_user.email.clone(),
-            password: new_user.password.clone()
-        }
-    }
-}
-
-#[derive(Clone, Default)]
-struct Profile {
-    profile_views: u32,
-    website_clicks: u32,
-}
-
-#[Object]
-impl Profile {
-    #[graphql(name = "profile_views")]
-    async fn profile_views(&self) -> u32 {
-        self.profile_views
-    }
-
-    /// would convert to camcel case websiteClicks in graphql query
-    /// 对于枚举类型，graphql会转为js风格例如FILE_A
-    async fn website_clicks(&self) -> u32 {
-        self.website_clicks
-    }
-
 
     // FIXME panic at runtime
-    /*
-    {
-        {
-            profiles {
-                profile_views,
-                # 注意struct中的字段会自动转为驼峰
-                websiteClicks
-        }
-    }
-    */
     // async fn profiles(&self, ctx: &Context<'_>) -> Vec<Profile> {
     //     let data = ctx.data::<Arc<Mutex<Vec<Profile>>>>().unwrap().lock().await;
     //     let res = data.clone();
@@ -79,18 +36,14 @@ impl Profile {
     // }
 }
 
-#[derive(async_graphql::MergedObject, Default)]
-struct QueryRoot(MyFirstQuery, Profile);
-
-type Data = Arc<Mutex<Vec<Profile>>>;
-
 #[derive(async_graphql::InputObject)]
 struct NewUser {
     email: String,
     password: String
 }
 
-#[derive(async_graphql::SimpleObject)]
+/// a SimpleObject can't have resolver function(因为自动生成了各字段的getter方法)
+#[derive(async_graphql::SimpleObject, Default)]
 struct User {
     id: u64,
     email: String,
@@ -99,19 +52,28 @@ struct User {
 
 // struct MutationRoot;
 
+// struct Subscription;
+//
+// #[async_graphql::Subscription]
+// impl Subscription {
+//     async fn integers(&self) -> impl futures::Stream<Item = i32> {
+//         futures::stream::once(async move { 10 })
+//         // tokio::time::interval(std::time::Duration::from_secs(1)).map(move |_| {
+//         //     value += 1;
+//         //     value
+//         // })
+//     }
+// }
+
 #[tokio::main]
 async fn main() -> DynError {
-    let profile_data = vec![Profile {
-        profile_views: 12389,
-        website_clicks: 322193,
-    }];
-    let data: Data = Arc::new(Mutex::new(profile_data));
+    // let data = Arc::new(Mutex::new(profile_data));
     let schema = Schema::build(
-        QueryRoot::default(),
-        async_graphql::EmptyMutation,
-        async_graphql::EmptySubscription,
+        QueryRoot,
+        EmptyMutation,
+        EmptySubscription,
     )
-    .data(data)
+    // .data(data)
     .finish();
 
     let graphql_post = async_graphql_warp::graphql(schema).and_then(
@@ -127,8 +89,7 @@ async fn main() -> DynError {
             .body(playground_source(GraphQLPlaygroundConfig::new("/")))
     });
 
-    let routes = graphql_playground
-        .or(graphql_post)
+    let routes = graphql_playground.or(graphql_post)
         .recover(|err: Rejection| async move {
             if let Some(BadRequest(err)) = err.find() {
                 return Ok::<_, Infallible>(warp::reply::with_status(
@@ -137,16 +98,15 @@ async fn main() -> DynError {
                 ));
             }
 
+            // hide server error to client
             Ok(warp::reply::with_status(
                 "INTERNAL_SERVER_ERROR".to_string(),
                 StatusCode::INTERNAL_SERVER_ERROR,
             ))
         });
 
+    println!("http://localhost:8003");
     warp::serve(routes).run(([0, 0, 0, 0], 8003)).await;
-
-
-
 
     Ok(())
 }
